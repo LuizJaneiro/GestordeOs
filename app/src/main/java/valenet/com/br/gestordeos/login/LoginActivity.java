03b30669f8898d8e0ebae0576ff6809b14748a62
Luiz Janeiro <luiz.janeiro@usemobile.xyz>
2018-04-12 16:22:38 -0300

    Login:  - Check empty fields  - Changed keyboard input method for Usuario

--- app/src/main/java/valenet/com/br/gestordeos/login/LoginActivity.java
+++ app/src/main/java/valenet/com/br/gestordeos/login/LoginActivity.java
@@ -3,12 +3,14 @@ package valenet.com.br.gestordeos.login;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.design.widget.TextInputLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.AppCompatButton;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.EditText;
-import android.widget.RelativeLayout;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -18,7 +20,6 @@ import butterknife.BindView;
 import butterknife.ButterKnife;
 import butterknife.OnClick;
 import es.dmoral.toasty.Toasty;
-import io.realm.Realm;
 import uk.co.chrisjenx.calligraphy.CalligraphyContextWrapper;
 import valenet.com.br.gestordeos.R;
 import valenet.com.br.gestordeos.forgot_password.ForgotPasswordActivity;
@@ -41,6 +42,10 @@ public class LoginActivity extends AppCompatActivity implements Login.LoginView 
     LottieAnimationView lottieAnimationLoading;
     @BindView(R.id.loading_view)
     ViewGroup loadingView;
+    @BindView(R.id.text_input_email)
+    TextInputLayout textInputEmail;
+    @BindView(R.id.text_input_password)
+    TextInputLayout textInputPassword;
 
     private Login.LoginPresenter presenter;
 
@@ -50,6 +55,40 @@ public class LoginActivity extends AppCompatActivity implements Login.LoginView 
         setContentView(R.layout.activity_login);
         ButterKnife.bind(this);
         this.presenter = new LoginPresenterImp(this);
+
+        editTextEmail.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                textInputEmail.setError(null);
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
+            }
+        });
+
+        editTextPassword.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                textInputPassword.setError(null);
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
+            }
+        });
 
         ClickGuard.guard(btnEntrar);
         ClickGuard.guard(textViewForgotPassword);
@@ -75,6 +114,8 @@ public class LoginActivity extends AppCompatActivity implements Login.LoginView 
                 navigateToForgotPassword();
                 break;
             case R.id.btn_entrar:
+                textInputEmail.setError(null);
+                textInputPassword.setError(null);
                 presenter.login(editTextEmail.getText().toString(),
                         editTextPassword.getText().toString());
                 break;
@@ -88,12 +129,14 @@ public class LoginActivity extends AppCompatActivity implements Login.LoginView 
 
     @Override
     public void showErrorEmptyUser() {
-
+        textInputEmail.requestFocus();
+        textInputEmail.setError(getResources().getString(R.string.error_field_required));
     }
 
     @Override
     public void showErrorEmptyPassword() {
-
+        textInputPassword.requestFocus();
+        textInputPassword.setError(getResources().getString(R.string.error_field_required));
     }
 
     @Override
