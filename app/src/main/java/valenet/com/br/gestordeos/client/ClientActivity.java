35234b0b53eebf6dbd146c312429d2ed77320b9d
Luiz Janeiro <luiz.janeiro@usemobile.xyz>
2018-04-19 16:52:31 -0300

    Client:  - Implemented AlertDialog for calls, and new activity with the phone number.

--- app/src/main/java/valenet/com/br/gestordeos/client/ClientActivity.java
+++ app/src/main/java/valenet/com/br/gestordeos/client/ClientActivity.java
@@ -1,9 +1,17 @@
 package valenet.com.br.gestordeos.client;
 
 import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
 import android.support.design.widget.TabLayout;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v4.view.ViewPager;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.AppCompatButton;
 import android.support.v7.widget.Toolbar;
@@ -102,6 +110,11 @@ public class ClientActivity extends AppCompatActivity {
         tabLayout.addTab(tabLayout.newTab().setText("Observações"));
         tabLayout.setTabGravity(TabLayout.GRAVITY_FILL);
 
+        if(os.getTelefoneCliente() == null) {
+            btnCall.setEnabled(false);
+            btnCall.setBackgroundColor(getResources().getColor(R.color.btn_call_transparent));
+        }
+
         pagerAdapter = new PagerAdapter(getSupportFragmentManager(), os, tabLayout.getTabCount());
         pager.setAdapter(pagerAdapter);
         pager.addOnPageChangeListener(new TabLayout.TabLayoutOnPageChangeListener(tabLayout));
@@ -135,19 +148,62 @@ public class ClientActivity extends AppCompatActivity {
         }
     }
 
-    @Override
-    protected void attachBaseContext(Context newBase) {
-        super.attachBaseContext(CalligraphyContextWrapper.wrap(newBase));
-    }
+    public void callPhone(final String phone) {
+        android.app.AlertDialog.Builder builderCad;
+        builderCad = new android.app.AlertDialog.Builder(this);
+        builderCad.setTitle("Atenção");
+        builderCad.setMessage("Deseja realizar a ligação?");
+        builderCad.setPositiveButton("Sim", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                dialog.dismiss();
+                Intent intent;
+                intent = new Intent(Intent.ACTION_DIAL);
+                intent.setData(Uri.parse("tel:" + phone));
+                // intent.setData(Uri.parse("tel:" + "03131975107000"));
 
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M)
+                    if (ContextCompat.checkSelfPermission(ClientActivity.this, android.Manifest.permission.CALL_PHONE) == PackageManager.PERMISSION_GRANTED) {
+                        startActivity(intent);
+                    } else {
+                        ActivityCompat.requestPermissions(ClientActivity.this, new String[]{android.Manifest.permission.CALL_PHONE}, 0);
+                    }
+                else
+                    startActivity(intent);
+
+            }
+        });
+        builderCad.setNegativeButton("Não", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                dialog.dismiss();
+            }
+        });
+        final android.app.AlertDialog dialog = builderCad.create();
+        dialog.setOnShowListener(new DialogInterface.OnShowListener() {
+            @Override
+            public void onShow(DialogInterface arg0) {
+                dialog.getButton(AlertDialog.BUTTON_NEGATIVE).setTextColor(getResources().getColor(R.color.btn_negative_dialog));
+                dialog.getButton(AlertDialog.BUTTON_POSITIVE).setTextColor(getResources().getColor(R.color.btn_positive_dialog));}
+        });
+        dialog.show();
+
+    }
 
     @OnClick({R.id.btn_call, R.id.footer_layout})
     public void onViewClicked(View view) {
         switch (view.getId()) {
             case R.id.btn_call:
+                this.callPhone(os.getTelefoneCliente()+"");
                 break;
             case R.id.footer_layout:
                 break;
         }
     }
+
+    @Override
+    protected void attachBaseContext(Context newBase) {
+        super.attachBaseContext(CalligraphyContextWrapper.wrap(newBase));
+    }
+
 }
